f4852db87924b99bb8ac2d2e5fc27c17526ec7fd
Bruno Bonacci <bruno.bonacci@gmail.com>
2015-11-27 11:36:01 +0000

    pinning new version of cider to stable

---
+++ personal/preload/general.el
@@ -0,0 +1,6 @@
+;; Pinning cider to stable version
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
