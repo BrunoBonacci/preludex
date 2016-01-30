3f23a98239ea77b5824c41b8aeda98e6233f03f8
Bruno Bonacci <bruno.bonacci@gmail.com>
2016-01-30 22:07:15 +0000

    change tags color in org-mode

--- personal/preload/theme.el
+++ personal/preload/theme.el
@@ -7,7 +7,7 @@
 ;;(if (require 'solarized-theme nil 'noerror)
 ;;  (setq prelude-theme 'solarized-dark)
 ;;  (setq prelude-theme nil))
-;;(setq solarized-use-variable-pitch nil)
+(setq solarized-use-variable-pitch nil)
 
 (set-face-attribute 'default nil :height 140)
 ;; hightlight modeline of active buffer
@@ -15,6 +15,14 @@
 (set-face-background 'mode-line "green4")
 (set-face-background 'modeline-inactive "grey20")
 (set-face-foreground 'mode-line-buffer-id "white")
+
+;; highligh tags
+(custom-set-faces
+ '(org-tag
+   ((t (:foreground "goldenrod"
+        ;;:box (:line-width 1 :color "DarkGoldenrod4")
+        :weight regular)))))
+
 
 ;; disable scroll bars
 (if (display-graphic-p)
