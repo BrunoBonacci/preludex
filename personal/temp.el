617b66edd23dc8809c8c981ed92cfc6efd278b53
Bruno Bonacci <bruno.bonacci@gmail.com>
2016-05-18 20:32:17 +0100

    incanter hack

--- personal/temp.el
+++ personal/temp.el
@@ -25,3 +25,45 @@
              (figwheel-sidecar.repl-api/start-figwheel!) ; idempotent
              (figwheel-sidecar.repl-api/cljs-repl)")
     (cider-repl-return)))
+
+
+
+;;
+;; Incanter eval and display chart
+;;
+
+(require 'cider)
+(setq incanter-temp-chart-file "/tmp/chart.png")
+(setq incanter-wait-time 500)
+
+
+(defun incanter-display-image-inline (buffer-name file-name)
+  "Use `BUFFER-NAME' to display the image in `FILE-NAME'.
+  Checks weather `BUFFER-NAME' already exists, and if not create
+  as needed."
+  (save-excursion
+    (switch-to-buffer-other-window buffer-name)
+    (iimage-mode t)
+    (read-only-mode -1)
+    (kill-region (point-min) (point-max))
+    ;; unless we clear the cache, the same cached image will
+    ;; always get re-displayed.
+    (clear-image-cache nil)
+    (insert-image (create-image file-name))
+    (read-only-mode t)))
+
+
+(defun incanter-eval-and-display-chart ()
+  "Evaluate the expression preceding point
+   and display the chart into a popup buffer"
+  (interactive)
+  (save-excursion
+    (condition-case nil
+        (delete-file incanter-temp-chart-file)
+      (error nil))
+    (cider-eval-last-sexp)
+    (sleep-for 0 incanter-wait-time)
+    (incanter-display-image-inline "*incanter-chart*" incanter-temp-chart-file)))
+
+(define-key cider-mode-map
+  (kbd "C-c C-i") #'incanter-eval-and-display-chart)
