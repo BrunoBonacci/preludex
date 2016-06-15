836beacfce087b36a44324edc690c0361a31a043
Bruno Bonacci <bruno.bonacci@gmail.com>
2016-06-15 10:03:10 +0100

    fixed emacs-incanter hack

--- personal/temp.el
+++ personal/temp.el
@@ -53,7 +53,6 @@
   "Use `BUFFER-NAME' to display the image in `FILE-NAME'.
   Checks weather `BUFFER-NAME' already exists, and if not create
   as needed."
-  (save-excursion
     (switch-to-buffer-other-window buffer-name)
     (iimage-mode t)
     (read-only-mode -1)
@@ -62,24 +61,23 @@
     ;; always get re-displayed.
     (clear-image-cache nil)
     (insert-image (create-image file-name))
-    (read-only-mode t)))
-
+  (read-only-mode t))
 
 (defun incanter-eval-and-display-chart ()
   "Evaluate the expression preceding point
    and display the chart into a popup buffer"
   (interactive)
-  (save-excursion
+  (let ((old-buf (current-buffer)))
     (condition-case nil
         (delete-file incanter-temp-chart-file)
       (error nil))
     (cider-eval-last-sexp)
     (sleep-for 0 incanter-wait-time)
-    (incanter-display-image-inline "*incanter-chart*" incanter-temp-chart-file)))
+    (incanter-display-image-inline "*incanter-chart*" incanter-temp-chart-file)
+    (switch-to-buffer-other-window old-buf)))
 
 (define-key cider-mode-map
   (kbd "C-c C-i") #'incanter-eval-and-display-chart)
-
 
 ;;
 ;; CIDER repl evaluation with output in comment
