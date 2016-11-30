7c3ece7f677cd64fbc54191fae68b5e4be960e3f
Bruno Bonacci <bruno.bonacci@gmail.com>
2016-11-30 15:44:16 +0000

    helm fuzzy matching

--- personal/general-conf.el
+++ personal/general-conf.el
@@ -245,7 +245,11 @@
 ;; Using Helm by default everywhere
 ;;
 (require 'prelude-helm-everywhere)
-(setq helm-M-x-fuzzy-match t)
+(setq helm-M-x-fuzzy-match        t
+      helm-semantic-fuzzy-match   t
+      helm-imenu-fuzzy-match      t
+      helm-buffers-fuzzy-matching t
+      helm-recentf-fuzzy-match    t)
 
 
 ;;
