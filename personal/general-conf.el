a8c89acef114c058222ce78afca33efb40878fd9
Bruno Bonacci <bruno.bonacci@gmail.com>
2018-02-23 11:44:19 +0000

    install flymd (for markdown preview on-the-fly)

--- personal/general-conf.el
+++ personal/general-conf.el
@@ -350,6 +350,22 @@
 ;;
 (prelude-require-package 'transpose-frame)
 
+
+;;
+;; flymd installation
+;;
+(prelude-require-package 'flymd)
+(require 'flymd)
+(defun my-flymd-browser-function (url)
+  (let ((process-environment (browse-url-process-environment)))
+    (apply 'start-process
+           (concat "firefox " url)
+           nil
+           "/usr/bin/open"
+           (list "-a" "firefox" url))))
+(setq flymd-browser-open-function 'my-flymd-browser-function)
+
+
 ;;;
 ;;; general-conf.el ends here
 ;;;
