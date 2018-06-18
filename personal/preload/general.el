5109a03d61e61b1cac4919516c15d3553ccb80ae
Bruno Bonacci <bruno.bonacci@gmail.com>
2018-06-18 10:15:29 +0100

    pin cider and clj-refactoring to stable version

--- personal/preload/general.el
+++ personal/preload/general.el
@@ -3,4 +3,9 @@
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
-;;(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
+;;
+;; Pin CIDER and clj-refactor to stable versions
+;; *comment this for the latest development versions*
+;;
+(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
+(add-to-list 'package-pinned-packages '(clj-refactor . "melpa-stable") t)
