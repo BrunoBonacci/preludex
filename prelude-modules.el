6e0f67cf61742534fd5ca2509ac185402723e35d
Bruno Bonacci <bruno.bonacci@gmail.com>
2018-06-19 11:06:14 +0100

    removed mediawiki module

--- prelude-modules.el
+++ prelude-modules.el
@@ -8,7 +8,7 @@
 ;; (require 'prelude-helm-everywhere) ;; Enable Helm everywhere
 (require 'prelude-company)
 (require 'prelude-key-chord) ;; Binds useful features to key combinations
-(require 'prelude-mediawiki)
+;; (require 'prelude-mediawiki)
 ;; (require 'prelude-evil)
 
 ;;; Programming languages support
